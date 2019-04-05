4e04c050a1406769a933768e232341e58d73de28
Dan Federman <dfed@me.com>
2019-04-04 18:43:54 -0700

    Attempt to break ci job into a script

---
+++ Scripts/ci.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -l
+set -e
+
+if [ -n "$DESTINATION" ]; then
+  xcodebuild -project Valet.xcodeproj -scheme "$SCHEME" -sdk $SDK -destination "$DESTINATION" -configuration Debug -PBXBuildsContinueAfterErrors=0 $ACTION
+else
+  xcodebuild -project Valet.xcodeproj -scheme "$SCHEME" -sdk $SDK -configuration Debug -PBXBuildsContinueAfterErrors=0 $ACTION
+fi
