faaa5ba47c15e2f4fb13ced4d7368db2f386db80
Jake Holland <jholland@jimmyjohns.com>
2019-04-12 10:11:14 -0500

    Update CI

--- Scripts/ci.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -l
-set -ex
-
-if [ -n "$DESTINATION" ]; then
-  xcodebuild -project Valet.xcodeproj -scheme "$SCHEME" -sdk $SDK -destination "$DESTINATION" -configuration Debug -PBXBuildsContinueAfterErrors=0 $ACTION
-else
-  xcodebuild -project Valet.xcodeproj -scheme "$SCHEME" -sdk $SDK -configuration Debug -PBXBuildsContinueAfterErrors=0 $ACTION
-fi
