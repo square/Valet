dd1c01ad1463c5dfedd8a3944ec5dc0568d7cdea
Dan Federman <dan.federman@airbnb.com>
2020-01-20 10:47:47 -0800

    Add explicit tests for CloudAccessibility

---
+++ Tests/ValetTests/CloudAccessibilityTests.swift
@@ -0,0 +1,37 @@
+//  Created by Dan Federman on 1/20/20.
+//  Copyright © 2020 Square, Inc.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import Foundation
+import XCTest
+
+@testable import Valet
+
+
+class CloudAccessibilityTests: XCTestCase {
+
+    func test_description_mirrorsAccessibilityCounterpartDescription() {
+        CloudAccessibility.allValues().forEach {
+            XCTAssertEqual($0.description, $0.accessibility.description)
+        }
+    }
+
+    func test_secAccessibilityAttribute_mirrorsAccessibilityCounterpartSecAccessibilityAttribute() {
+        CloudAccessibility.allValues().forEach {
+            XCTAssertEqual($0.secAccessibilityAttribute, $0.accessibility.secAccessibilityAttribute)
+        }
+    }
+
+}
