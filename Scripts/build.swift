756eb170510ae42c73758f09e03f6fa9503002c6
Jon Shier <jon@jonshier.com>
2023-06-06 15:09:56 -0400

    New OS versions.

--- Scripts/build.swift
+++ Scripts/build.swift
@@ -25,18 +25,22 @@ enum Platform: String, CustomStringConvertible {
     case iOS_14
     case iOS_15
     case iOS_16
+    case iOS_17
     case tvOS_13
     case tvOS_14
     case tvOS_15
     case tvOS_16
+    case tvOS_17
     case macOS_10_15
     case macOS_11
     case macOS_12
     case macOS_13
+    case macOS_14
     case watchOS_6
     case watchOS_7
     case watchOS_8
     case watchOS_9
+    case watchOS_10
 
     var destination: String {
         switch self {
@@ -48,6 +52,8 @@ enum Platform: String, CustomStringConvertible {
             return "platform=iOS Simulator,OS=15.5,name=iPad Pro (12.9-inch) (5th generation)"
         case .iOS_16:
             return "platform=iOS Simulator,OS=16.4,name=iPad Pro (12.9-inch) (6th generation)"
+        case .iOS_17:
+            return "platform=iOS Simulator,OS=17.0,name=iPad Pro (12.9-inch) (6th generation)"
 
         case .tvOS_13:
             return "platform=tvOS Simulator,OS=13.4,name=Apple TV"
@@ -57,11 +63,14 @@ enum Platform: String, CustomStringConvertible {
             return "platform=tvOS Simulator,OS=15.4,name=Apple TV"
         case .tvOS_16:
             return "platform=tvOS Simulator,OS=16.4,name=Apple TV"
+        case .tvOS_17:
+            return "platform=tvOS Simulator,OS=17.0,name=Apple TV"
 
         case .macOS_10_15,
              .macOS_11,
              .macOS_12,
-             .macOS_13:
+             .macOS_13,
+             .macOS_14:
             return "platform=OS X"
 
         case .watchOS_6:
@@ -72,6 +81,8 @@ enum Platform: String, CustomStringConvertible {
             return "OS=8.5,name=Apple Watch Series 6 - 44mm"
         case .watchOS_9:
             return "OS=9.4,name=Apple Watch Series 6 - 44mm"
+        case .watchOS_10:
+            return "OS=10.0,name=Apple Watch Series 6 - 44mm"
         }
     }
 
@@ -80,13 +91,15 @@ enum Platform: String, CustomStringConvertible {
         case .iOS_13,
              .iOS_14,
              .iOS_15,
-             .iOS_16:
+             .iOS_16,
+             .iOS_17:
             return "iphonesimulator"
 
         case .tvOS_13,
              .tvOS_14,
              .tvOS_15,
-             .tvOS_16:
+             .tvOS_16
+             .tvOS_17:
             return "appletvsimulator"
 
         case .macOS_10_15:
@@ -97,11 +110,14 @@ enum Platform: String, CustomStringConvertible {
             return "macosx12.3"
         case .macOS_13:
             return "macosx13.3"
+        case .macOS_14:
+            return "macosx14.0"
 
         case .watchOS_6,
              .watchOS_7,
              .watchOS_8,
-             .watchOS_9:
+             .watchOS_9,
+             .watchOS_10:
             return "watchsimulator"
         }
     }
@@ -112,20 +128,24 @@ enum Platform: String, CustomStringConvertible {
              .iOS_14,
              .iOS_15,
              .iOS_16,
+             .iOS_17,
              .tvOS_13,
              .tvOS_14,
              .tvOS_15,
              .tvOS_16,
+             .tvOS_17,
              .macOS_10_15,
              .macOS_11,
              .macOS_12,
-             .macOS_13:
+             .macOS_13,
+             .macOS_14:
             return true
 
         case .watchOS_6,
              .watchOS_7,
              .watchOS_8,
-             .watchOS_9:
+             .watchOS_9,
+             .watchOS_10:
             // watchOS does not support unit testing (yet?).
             return false
         }
@@ -140,25 +160,29 @@ enum Platform: String, CustomStringConvertible {
         case .iOS_13,
              .iOS_14,
              .iOS_15,
-             .iOS_16:
+             .iOS_16,
+             .iOS_17:
             return "Valet iOS"
 
         case .tvOS_13,
              .tvOS_14,
              .tvOS_15,
-             .tvOS_16:
+             .tvOS_16,
+             .tvOS_17:
             return "Valet tvOS"
 
         case .macOS_10_15,
              .macOS_11,
              .macOS_12,
-             .macOS_13:
+             .macOS_13,
+             .macOS_14:
             return "Valet Mac"
 
         case .watchOS_6,
              .watchOS_7,
              .watchOS_8,
-             .watchOS_9:
+             .watchOS_9,
+             .watchOS_10:
             return "Valet watchOS"
         }
     }
