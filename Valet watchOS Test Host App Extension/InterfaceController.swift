a1cbab7c25effde6ff88359ff4322b5e08a1b7e8
Dan Federman <dfed@me.com>
2024-04-22 22:40:49 -0700

    Get watchOS tests running in CI

--- Valet watchOS Test Host App Extension/InterfaceController.swift
+++ Valet watchOS Test Host App Extension/InterfaceController.swift
@@ -16,19 +16,6 @@
 
 import WatchKit
 import Foundation
-import XCTest
 
 
-class InterfaceController: WKInterfaceController {
-
-    override func didAppear() {
-        super.didAppear()
-
-        if XCTestCase.runAllTests() {
-            setTitle("Success!")
-        } else {
-            setTitle("Failure!")
-        }
-    }
-
-}
+class InterfaceController: WKInterfaceController {}
