058c73e96fefc1b8ffbfe209555a76c5edc5370d
Dan Federman <dfed@me.com>
2024-04-23 00:18:44 -0700

    simpler

--- Valet watchOS Test Host App Watch App/ContentView.swift
+++ Valet watchOS Test Host App Watch App/ContentView.swift
@@ -19,14 +19,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
             Text("Hello, world!")
         }
         .padding()
     }
-}
-
-#Preview {
-    ContentView()
 }
